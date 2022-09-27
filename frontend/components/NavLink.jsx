3156e707b169ae61ea3f627a762a835259a771ed
Akuya Ekorot <akuyaekorot@gmail.com>
2022-09-27 13:10:37 +0300

    build basic structure of nav bar

---
+++ frontend/components/NavLink.jsx
@@ -0,0 +1,7 @@
+export default function NavLink({ link }) {
+  return (
+    <div className="p-7">
+      <p className="text-p2 text-akuya-5">{link}</p>
+    </div>
+  );
+}
--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,7 +1,24 @@
+import NavLink from "../components/NavLink";
+
 export default function Home() {
+  const links = ["Services", "Work", "Case Studies", "Blog", "About Me"];
   return (
-    <>
-      <h1 className="font-mono text-p1">Hello Next</h1>
-    </>
+    <div className="bg-akuya-1 flex flex-col items-stretch h-screen w-screen">
+      <nav className="flex justify-center w-screen">
+        <div className="flex flex-row justify-between items-center w-full max-w-7xl">
+          <div className="px-2.5">
+            <p className="font-serif text-h2">Akuya</p>
+          </div>
+          <div className="flex flex-row items-center">
+            {links.map((navLink) => {
+              return <NavLink link={navLink} />;
+            })}
+          </div>
+          <div className="px-5 py-2.5 bg-accent text-akuya-1">
+            <a>Hire Me</a>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
