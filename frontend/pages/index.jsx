b5448a3d0c34c7c14b61e7ede975140632cdc580
Akuya Ekorot <akuyaekorot@gmail.com>
2022-09-30 23:27:21 +0300

    refactor hero section into a component

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -4,6 +4,7 @@ import NavBar from "../components/NavBar";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import Button from "../components/Button";
+import Hero from "../components/Hero";
 
 export default function Home({ navLinks, heading, services }) {
   const [count, setCount] = useState(0);
@@ -29,41 +30,12 @@ export default function Home({ navLinks, heading, services }) {
   return (
     <div className="bg-akuya-1 flex flex-col items-center gap-[60px] h-screen w-screen">
       <NavBar links={navLinks} />
-      <section className="flex flex-row items-stretch w-full max-w-7xl">
-        <div className="flex-grow flex flex-col gap-[60px]">
-          <h1 className="text-h1 font-serif w-[500px]">
-            {heading}
-            <br />
-            <span className="text-accent" ref={countRef}>
-              {services[count]?.attributes.heroTitle}.
-            </span>
-          </h1>
-          <div className="flex flex-row gap-[30px] w-full">
-            <Button
-              text={`Hire Me`}
-              bgColor={`akuya-6`}
-              textColor={`akuya-1`}
-              link={`hire-me`}
-              hero
-            />
-            <Button
-              text={`Learn More`}
-              bgColor={`akuya-1`}
-              textColor={`akuya-7`}
-              link={`services`}
-              hero
-              border
-            />
-          </div>
-        </div>
-        <div className="relative flex-grow h-[500px] w-[500px]">
-          <Image
-            src={`${services[count]?.attributes.imageUrl}`}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-      </section>
+      <Hero
+        services={services}
+        countRef={countRef}
+        heading={heading}
+        count={count}
+      />
     </div>
   );
 }
