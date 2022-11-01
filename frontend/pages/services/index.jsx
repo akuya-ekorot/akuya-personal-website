d9e98a64ab163fdede4881becc996c1b482ab915
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-02 02:36:12 +0300

    add navigation bar to services page

--- frontend/pages/services/index.jsx
+++ frontend/pages/services/index.jsx
@@ -1,5 +1,65 @@
-const Services = () => {
-  return <>Hello World</>;
+import NavBar from "../../components/NavBar";
+import client from "../../lib/apollo";
+import { gql } from "@apollo/client";
+
+const Services = (props) => {
+  return (
+    <>
+      <NavBar links={props?.navLinks} />
+    </>
+  );
 };
 
 export default Services;
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query {
+        navLinks {
+          data {
+            attributes {
+              name
+              url
+            }
+          }
+        }
+        hero {
+          data {
+            attributes {
+              heading
+              services {
+                data {
+                  attributes {
+                    title
+                    caption
+                    heroTitle
+                    imageUrl
+                    slug
+                  }
+                }
+              }
+            }
+          }
+        }
+        serviceSection {
+          data {
+            attributes {
+              title
+              paragraph
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      navLinks: data.navLinks.data,
+      heading: data.hero.data.attributes.heading,
+      services: data.hero.data.attributes.services.data,
+      serviceSection: data["serviceSection"].data.attributes,
+    },
+  };
+}
